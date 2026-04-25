cc673ce1696fdb8ecf88f1640ac2f9db224c9fd3
dumbogea2 <dumbogea2@gmail.com>
2026-04-25 11:02:51 +0200

    pwaPowerWebApp

---
+++ sw.js
@@ -0,0 +1,142 @@
+const CACHE_NAME = 'valtorta-cache-v3'; // Versione aggiornata per forzare il ricaricamento
+
+// 1. FILE FONDAMENTALI (Scaricati subito durante l'installazione)
+const urlsToCache = [
+  // --- PAGINE PRINCIPALI E SCRIPT ---
+  './index.html',
+  './login.html',
+  './pwa_info.html',
+  './istruzioni.html',
+  './mia_cronologia.html',
+  './miei_appunti.html',
+  './ricerca_universale.html',
+  './ricerca.html',
+  './archivio.html',
+  './cronologia.js',
+  './sincronizzazione.js',
+  './appunti.js',
+
+  // --- IMMAGINI E SFONDI DELLA HOME ---
+  './sfondo.png',
+  './sfondo2.png',
+  './home_sfondo.png',
+  './EvangeloButton.png',
+  './mappa-pergamena.png',
+  './QuaderniButtonimg.jpg',
+  './libro_azaria_copertina.png',
+  './imgButtonRomani.png',
+  './AutobiografiaButton.png',
+  './ricerca_universale_button.png',
+  './ArchivioButton.jpg',
+
+  // --- MAPPA (Librerie e icone di base) ---
+  './mappa/index.html',
+  './mappa/leaflet/leaflet.css',
+  './mappa/leaflet/leaflet.js',
+  './mappa/img/citta.svg',
+  './mappa/img/acqua.svg',
+  './mappa/img/sacro.svg',
+
+  // --- BIBLIOTECA PRINCIPALE ---
+  './biblioteca/index.html',
+  './biblioteca/archivio.js',
+  './biblioteca/sfondo.png',
+  './biblioteca/handmadepaper.png',
+
+  // --- QUADERNI E OPERE MINORI ---
+  './Quaderni/home.html',
+  './Quaderni/Quaderni-home.png',
+  './Quaderni/handmadepaper.png',
+  
+  './Quaderni/quaderni_1943.html',
+  './Quaderni/archivio_1943.js',
+  './Quaderni/Quaderni1.png',
+  
+  './Quaderni/quaderni_1944.html',
+  './Quaderni/archivio_1944.js',
+  './Quaderni/Quaderni2.png',
+  
+  './Quaderni/quaderni_1945-1950.html',
+  './Quaderni/archivio_1945-1950.js',
+  './Quaderni/Quaderni3.png',
+  
+  './Quaderni/quadernetti.html',
+  './Quaderni/Quadernetti.js',
+  './Quaderni/Quadernetti.png',
+  
+  './Quaderni/libro_azaria.html',
+  './Quaderni/archivio_azaria.js',
+  './Quaderni/imgAzaria.png',
+  
+  './Quaderni/lezioni_romani.html',
+  './Quaderni/archivio_romani.js',
+  './Quaderni/imgRomani.png',
+  
+  './Quaderni/autobiografia.html',
+  './Quaderni/Autobiografia.js',
+  './Quaderni/Autobiografia.png',
+
+  // --- PRESENTAZIONE ---
+  './presentazione/index.html'
+];
+
+// Installazione: scarica i file fondamentali nella cache
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Attivazione: Pulisce le vecchie cache se cambiamo versione
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Gestione richieste: CACHING DINAMICO
+self.addEventListener('fetch', (event) => {
+  // Ignora richieste verso domini esterni (come Firebase) o estensioni Chrome
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 1. Se in cache, usa la cache
+        if (response) {
+          return response;
+        }
+
+        // 2. Se non c'è, scaricalo da internet e salvalo in background
+        return fetch(event.request).then(
+          (networkResponse) => {
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+
+            const responseToCache = networkResponse.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return networkResponse;
+          }
+        ).catch(() => {
+            // Se fallisce (es. utente offline e pagina mai visitata), non fare nulla per ora
+        });
+      })
+  );
+});
