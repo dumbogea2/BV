2a51bd2fd687adfbc2b23a2acf1fb751f913b3b7
dumbogea2 <dumbogea2@gmail.com>
2026-04-26 09:07:55 +0200

    upgradepwahome

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'valtorta-cache-v15'; // Versione aggiornata per forzare il ricaricamento
+const CACHE_NAME = 'valtorta-cache-v16'; // Versione aggiornata per forzare il ricaricamento
 
 // 1. FILE FONDAMENTALI (Scaricati subito durante l'installazione)
 const urlsToCache = [
