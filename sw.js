27e772989ef2305ec594439659905e3c3d42378f
dumbogea2 <dumbogea2@gmail.com>
2026-04-27 22:39:30 +0200

    uopgrade_sw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'valtorta-cache-v28'; // Versione aggiornata per forzare il ricaricamento
+const CACHE_NAME = 'valtorta-cache-v30'; // Versione aggiornata per forzare il ricaricamento
 
 // 1. FILE FONDAMENTALI (Scaricati subito durante l'installazione)
 const urlsToCache = [
