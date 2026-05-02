c2f75f0689813d5ac235d4d3838ec0e3bd142938
dumbogea2 <dumbogea2@gmail.com>
2026-05-02 21:01:35 +0200

    updated_ricerche

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'valtorta-cache-v33'; // Versione aggiornata per forzare il ricaricamento
+const CACHE_NAME = 'valtorta-cache-v35'; // Versione aggiornata per forzare il ricaricamento
 
 // 1. FILE FONDAMENTALI (Scaricati subito durante l'installazione)
 const urlsToCache = [
