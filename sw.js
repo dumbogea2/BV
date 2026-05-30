8f88d2b217263b22e29d54b6b5b14fed5dda1e02
dumbogea2 <dumbogea2@gmail.com>
2026-05-30 19:32:53 +0200

    note_e_back_ricerca_ugrade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'valtorta-cache-v91'; // Bump versione: aggiunte immagini VideoPresentazione_img.png e InstallApp_button.png
+const CACHE_NAME = 'valtorta-cache-v92'; // Bump versione: aggiunte immagini VideoPresentazione_img.png e InstallApp_button.png
 
 // 1. FILE FONDAMENTALI (Scaricati subito durante l'installazione)
 const urlsToCache = [
